073e0f3c3eccc43daf032e673a599a1e2e9221c4
Dan van Kley <dvankley@salesforce.com>
2018-09-03 17:01:55 -0400

    Enable trinamic hybrid mode and current monitoring

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1183,7 +1183,7 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 S0/1 - Report driver parameters (Requires TMC_DEBUG)
    */
-  //#define MONITOR_DRIVER_STATUS
+  #define MONITOR_DRIVER_STATUS
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
@@ -1197,7 +1197,7 @@
    * STEALTHCHOP needs to be enabled.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD
 
   #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
