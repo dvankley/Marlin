f95ee5d8fef798eba5818577900fcec1a67ab8ad
Dan van Kley <dvankley@salesforce.com>
2018-09-03 17:05:03 -0400

    Sometimes you get too used to autosaving IDEs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -565,7 +565,6 @@
  *          TMC5130, TMC5130_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'L6470', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC26X', 'TMC26X_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE']
  */
-<<<<<<< HEAD
 #define X_DRIVER_TYPE  TMC2130
 #define Y_DRIVER_TYPE  TMC2130
 #define Z_DRIVER_TYPE  TMC2130
@@ -577,19 +576,6 @@
 // #define E2_DRIVER_TYPE TMC2130
 // #define E3_DRIVER_TYPE TMC2130
 // #define E4_DRIVER_TYPE TMC2130
-=======
-//#define X_DRIVER_TYPE  A4988
-//#define Y_DRIVER_TYPE  A4988
-//#define Z_DRIVER_TYPE  A4988
-//#define X2_DRIVER_TYPE A4988
-//#define Y2_DRIVER_TYPE A4988
-//#define Z2_DRIVER_TYPE A4988
-//#define E0_DRIVER_TYPE A4988
-//#define E1_DRIVER_TYPE A4988
-//#define E2_DRIVER_TYPE A4988
-//#define E3_DRIVER_TYPE A4988
-//#define E4_DRIVER_TYPE A4988
->>>>>>> d62f4df0b7bfa8f9ca99f2ea5face12780b6a75f
 
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
